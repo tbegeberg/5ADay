727387d307b751ead5b7869c9c1fafeef7862efa
Tørk Egeberg <tbegeberg@gmail.com>
2018-01-16 16:28:03 +0100

    Added time and dates to measure if it is a new day

--- FiveADay/FiveADay/FiveADay/GameScene.swift
+++ FiveADay/FiveADay/FiveADay/GameScene.swift
@@ -14,17 +14,33 @@ class GameScene: SKScene {
     var selectedNode = SKSpriteNode()
     var nodeNameArray = [""]
     let monster = SKSpriteNode(imageNamed: "closeMonster")
+    var currentDay: Bool?
+    var label: UILabel?
+    var count: Int = 0
+    let userDefaults = UserDefaults.standard
     
     override func didMove(to view: SKView) {
         
         self.backgroundColor = SKColor.white
+        let date = Date()
         monster.size = CGSize(width: monster.size.width/2, height: monster.size.height/2)
         monster.position = CGPoint(x: self.frame.width/2, y: self.frame.height/2)
         monster.zPosition = -1
         monster.name = "monster"
         self.addChild(monster)
-        addFruit()
         addCarrot()
+        addFruit()
+        label = UILabel()
+        label?.translatesAutoresizingMaskIntoConstraints = false
+        label?.textAlignment = .center
+        if let countlabel = label {
+            self.view?.addSubview(countlabel)
+        }
+        label?.bottomAnchor.constraint(equalTo: (self.view?.bottomAnchor)!, constant: -50).isActive = true
+        label?.centerXAnchor.constraint(equalTo: (self.view?.centerXAnchor)!).isActive = true
+        dayDifference(from: userDefaults.double(forKey: "Date"))
+
+        userDefaults.set(date.timeIntervalSince1970, forKey: "Date")
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -34,8 +50,6 @@ class GameScene: SKScene {
         }
         let touchLocation = touch.location(in: self)
         selectNodeForTouch(touchLocation: touchLocation)
-        
-       
     }
     
     func degToRad(degree: Double) -> CGFloat {
@@ -58,7 +72,6 @@ class GameScene: SKScene {
                     selectedNode.run(SKAction.repeatForever(sequence))
                 }
             }
-            
         }
     }
     
@@ -99,17 +112,31 @@ class GameScene: SKScene {
         let isPointInFrame = monster.frame.contains(touchLocation)
 
         if isPointInFrame == true {
-            if selectedNode.name == "banana" {
-                selectedNode.removeFromParent()
-                delay(time: 2, closure: {
-                    self.addFruit()
-                })
-            }
-            if selectedNode.name == "carrot" {
-                selectedNode.removeFromParent()
-                delay(time: 2, closure: {
-                    self.addCarrot()
-                })
+    
+            switch self.selectedNode.name {
+                case "banana"?:
+                    delay(time: 2, closure: {
+                       self.addFruit()
+                    })
+                    selectedNode.removeFromParent()
+                    count += 1
+                case "carrot"?:
+                    delay(time: 2, closure: {
+                        self.addCarrot()
+                    })
+                    selectedNode.removeFromParent()
+                    count += 1
+                default:
+                    break
+                }
+            
+            label?.text = "Today count: \(count) "
+            userDefaults.set(count ,forKey: "eaten")
+
+            if count >= 5 {
+                label?.text = "YOU HAVE REACHED 5ADAY!"
+                label?.sizeToFit()
+                //label?.center.x = (self.view?.center.x)!
             }
             
             monster.texture = SKTexture(imageNamed: "closeMonster")
@@ -119,7 +146,7 @@ class GameScene: SKScene {
     func addCarrot() {
         let carrot = SKSpriteNode(imageNamed: "carrot")
         carrot.size = CGSize(width: carrot.frame.width/3, height: carrot.frame.height/3)
-        carrot.position = CGPoint(x: self.frame.maxX - carrot.frame.width/5 - 50, y: self.frame.maxY - carrot.frame.height/5 - 50)
+        carrot.position = CGPoint(x: self.frame.maxX - carrot.frame.width/3 - 50, y: monster.frame.maxY + 50)
         carrot.name = "carrot"
         if let name = carrot.name {
             nodeNameArray.append(name)
@@ -130,7 +157,7 @@ class GameScene: SKScene {
     func addFruit() {
         let banana = SKSpriteNode(imageNamed: "banana")
         banana.size = CGSize(width: banana.frame.width/3, height: banana.frame.height/3)
-        banana.position = CGPoint(x: self.frame.minX + banana.frame.width/5 + 50, y: self.frame.maxY - banana.frame.height/5 - 50)
+        banana.position = CGPoint(x: self.frame.minX + banana.frame.width/5 + 50, y: monster.frame.maxY + 50)
         banana.name = "banana"
         if let name = banana.name {
             nodeNameArray.append(name)
@@ -142,6 +169,30 @@ class GameScene: SKScene {
         
         DispatchQueue.main.asyncAfter(deadline: DispatchTime.now() + time) {
             closure()
+        }
+    }
+    
+    func dayDifference(from interval : TimeInterval)
+    {
+        let calendar = NSCalendar.current
+        let date = Date(timeIntervalSince1970: interval)
+        if calendar.isDateInToday(date) {
+            currentDay = true
+            count = userDefaults.integer(forKey: "eaten")
+            if count >= 5 {
+                label?.text = "YOU HAVE REACHED 5ADAY!"
+                label?.sizeToFit()
+                //label?.center.x = (self.view?.center.x)!
+            } else {
+                label?.text = "Today count: \(count) "
+                label?.sizeToFit()
+                //label?.center.x = (self.view?.center.x)!
+            }
+        } else {
+            currentDay = false
+            print("not Today")
+            label?.text = "Today count: \(count) "
+            label?.sizeToFit()
         }
     }
 }
--- FiveADay/FiveADay/FiveADay/GameViewController.swift
+++ FiveADay/FiveADay/FiveADay/GameViewController.swift
@@ -14,8 +14,6 @@ class GameViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-      
         let scene = GameScene(size: view.bounds.size)
         let skView = view as! SKView
         skView.showsFPS = true
